7c45f32552d2fa752fa3f5570c3cff2357f2cc70
Mike <nordic.savoie@gmail.com>
2016-07-18 08:29:37 -0600

    Create FantasyProject.py

---
+++ FantasyProject.py
@@ -0,0 +1,27 @@
+# inventory.py
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+def displayInventory(inventory):
+    print("Inventory:")
+    item_total = 0
+    for k, v in inventory.items():
+        # FILL IN THE CODE HERE
+        item_total = item_total + v.get(stuff,0)
+    
+    
+    print(str(displayInventory(allItems, 'arrow')) + 'arrow')
+    print("Total number of items: " + str(item_total))
+
+displayInventory(stuff)
+
+
+
+
+#
+#Inventory:
+#12 arrow
+#42 gold coin
+#1 rope
+#6 torch
+#1 dagger
+#Total number of items: 62
