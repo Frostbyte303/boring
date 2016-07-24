c5ac56f5de93690f7162c4051ea40510258f63a0
Mike <nordic.savoie@gmail.com>
2016-07-24 10:51:11 -0600

    Update FantasyProject.py

--- FantasyProject.py
+++ FantasyProject.py
@@ -4,17 +4,19 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 def displayInventory(inventory):
     print("Inventory:")
     item_total = 0
-    for k, v in inventory.items():
+    for key, value in inventory.items():
         # FILL IN THE CODE HERE
-        item_total = item_total + v.get(stuff,0)
-    
-    
-    print(str(displayInventory(allItems, 'arrow')) + 'arrow')
+        item_total = item_total + value
+        print (value,key)
+        
+        
+        
+        
+        
+        
     print("Total number of items: " + str(item_total))
 
 displayInventory(stuff)
-
-
 
 
 #
